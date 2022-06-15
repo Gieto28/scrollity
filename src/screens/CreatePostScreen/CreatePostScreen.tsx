e748a021671e251319a9fcd52a4588e362127391
gieto28 <28miguelangelo@gmail.com>
2022-06-15 18:00:26 +0100

    Added react image picker **ON GOING**

--- src/screens/CreatePostScreen/CreatePostScreen.tsx
+++ src/screens/CreatePostScreen/CreatePostScreen.tsx
@@ -1,8 +1,12 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
+  ButtonWrapper,
   CreateBody,
   CreateHeader,
   CreatePostWrapper,
+  ErrorLabel,
+  ImagePreview,
+  ImageWrapper,
   Label,
   LabelWrapper,
   SelectMediaText,
@@ -16,11 +20,17 @@ import {
 } from '../../components';
 import {CommonActions, useNavigation} from '@react-navigation/native';
 import {useForm} from 'react-hook-form';
-import {ImageSourcePropType} from 'react-native';
+import {Dimensions, ImageSourcePropType} from 'react-native';
 import {useAppSettings} from '../../context';
 import SelectDropdown from 'react-native-select-dropdown';
 import {yupResolver} from '@hookform/resolvers/yup';
 import {CreatePostModel, SchemaCreatePost} from '../../models';
+import ImagePicker, {
+  ImagePickerResponse,
+  launchCamera,
+  launchImageLibrary,
+} from 'react-native-image-picker';
+import {AppScrollView} from '../../styles/GlobalStyle';
 
 /**
  *
@@ -29,7 +39,20 @@ import {CreatePostModel, SchemaCreatePost} from '../../models';
 const CreatePostScreen = () => {
   const {theme} = useAppSettings();
   const [pickedCategory, setPickedCategory] = useState<string>('Other');
+  const [pickedMedia, setPickedMedia] = useState<string | null>(null);
 
+  const navigation = useNavigation();
+
+  //array of category
+  const categoryArray = [
+    {category: 'Funny', id: 0},
+    {category: 'Pet', id: 1},
+    {category: 'Help', id: 2},
+    {category: 'Other', id: 3},
+    {category: 'Random', id: 4},
+  ];
+
+  //form hook
   const {
     control,
     handleSubmit,
@@ -43,22 +66,55 @@ const CreatePostScreen = () => {
     ? require('../../assets/Images/arrow-left-dark-24.png')
     : require('../../assets/Images/arrow-left-light-24.png');
 
-  const navigation = useNavigation();
+  //
+  //
+  const [filePath, setFilePath] = useState({});
+  const [image, setImage] = useState<string | undefined>(
+    'https://via.placeholder.com/400',
+  );
+  const [imageHeight, setImageHeight] = useState<number | undefined>(400);
+  const [imageError, setImageError] = useState(false);
 
-  const categoryArray = [
-    {category: 'Funny', id: 0},
-    {category: 'Pet', id: 1},
-    {category: 'Help', id: 2},
-    {category: 'Other', id: 3},
-    {category: 'Random', id: 4},
-  ];
+  // : require('../../assets/Images/image_preview.png')
+  //
+  //
+  const handleSubmitMedia = async () => {
+    console.log('submitting media button is working');
 
-  const handleGoBack = () => {
-    navigation.dispatch(CommonActions.goBack());
+    imageError === true && setImageError(false);
+
+    const res: ImagePickerResponse = await launchImageLibrary({
+      mediaType: 'mixed',
+      maxWidth: 400,
+      videoQuality: 'low',
+      presentationStyle: 'popover',
+    });
+
+    if (res.errorCode) console.log(res.errorMessage);
+    if (res.didCancel) console.log(res.didCancel);
+
+    if (res!.assets![0].height! > 900) {
+      setImageError(true);
+      throw new Error('Image is too tall!');
+    }
+
+    const mediaValue = res!.assets!;
+
+    const uriPath = mediaValue![0].uri!;
+    setImage(uriPath!);
+    setImageHeight(mediaValue![0].height! ?? 400);
   };
 
-  const handleSubmitMedia = () => {
-    console.log('submitting media button is working');
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  const handleGoBack = () => {
+    navigation.dispatch(CommonActions.goBack());
   };
 
   const handleSelectOptions = (option: any, index: number) => {
@@ -70,77 +126,94 @@ const CreatePostScreen = () => {
   const handleSubmitPost = async (data: CreatePostModel) => {
     console.log('data when submitting post', data);
     console.log(pickedCategory);
+    console.log(pickedMedia);
   };
 
   return (
-    <CreatePostWrapper>
-      <CreateHeader>
-        <IconComponent
-          image={leftArrowIcon}
-          altText={'Click here to go back to the previous screen'}
-          onPress={handleGoBack}
-        />
-      </CreateHeader>
-      <LabelWrapper>
-        <Label>Upload Post</Label>
-      </LabelWrapper>
-      <CreateBody>
-        <InputTextComponent
-          placeholder={'Title...'}
-          value={''}
-          label="title"
-          controllerName={'title'}
-          control={control}
-          errors={errors.title}
-        />
-        <InputTextComponent
-          style={{height: 100, textAlignVertical: 'top'}}
-          multiline={true}
-          numberOfLines={4}
-          label="Description"
-          placeholder={'Description...'}
-          value={''}
-          controllerName={'description'}
-          control={control}
-          errors={errors.description}
-        />
-        <SelectWrapper>
-          <SelectMediaWrapper onPress={handleSubmitMedia}>
-            <SelectMediaText>Media</SelectMediaText>
-          </SelectMediaWrapper>
-          <SelectDropdown
-            defaultButtonText="Category"
-            data={categoryArray}
-            onSelect={(selectedItem, index) =>
-              handleSelectOptions(selectedItem, index)
-            }
-            buttonStyle={{
-              width: 130,
-              borderWidth: 1,
-              borderStyle: 'solid',
-              borderColor: theme.button.border,
-              backgroundColor: theme.screen.background,
-              borderRadius: 10,
-            }}
-            buttonTextAfterSelection={(selectedItem, index) => {
-              // text represented after item is selected
-              // if data array is an array of objects then return selectedItem.property to render after item is selected
-              return selectedItem.category;
-            }}
-            rowTextForSelection={(item, index) => {
-              // text represented for each item in dropdown
-              // if data array is an array of objects then return item.property to represent item in dropdown
-              return item.category;
+    <AppScrollView>
+      <CreatePostWrapper>
+        <CreateHeader>
+          <IconComponent
+            image={leftArrowIcon}
+            altText={'Click here to go back to the previous screen'}
+            onPress={handleGoBack}
+          />
+        </CreateHeader>
+        <LabelWrapper>
+          <Label>Upload Post</Label>
+        </LabelWrapper>
+        <CreateBody>
+          <InputTextComponent
+            placeholder={'Title...'}
+            value={''}
+            label="title"
+            controllerName={'title'}
+            control={control}
+            errors={errors.title}
+          />
+          <InputTextComponent
+            style={{height: 100, textAlignVertical: 'top'}}
+            multiline={true}
+            numberOfLines={4}
+            label="Description"
+            placeholder={'Description...'}
+            value={''}
+            controllerName={'description'}
+            control={control}
+            errors={errors.description}
+          />
+          <SelectWrapper>
+            <SelectMediaWrapper onPress={handleSubmitMedia}>
+              <SelectMediaText>Media</SelectMediaText>
+            </SelectMediaWrapper>
+            <SelectDropdown
+              defaultButtonText="Category"
+              data={categoryArray}
+              onSelect={(selectedItem, index) =>
+                handleSelectOptions(selectedItem, index)
+              }
+              buttonStyle={{
+                width: 130,
+                borderWidth: 1,
+                borderStyle: 'solid',
+                borderColor: theme.button.border,
+                backgroundColor: theme.screen.background,
+                borderRadius: 10,
+              }}
+              buttonTextStyle={{}}
+              buttonTextAfterSelection={(selectedItem, index) => {
+                // text represented after item is selected
+                // if data array is an array of objects then return selectedItem.property to render after item is selected
+                return selectedItem.category;
+              }}
+              rowTextForSelection={(item, index) => {
+                // text represented for each item in dropdown
+                // if data array is an array of objects then return item.property to represent item in dropdown
+                return item.category;
+              }}
+            />
+          </SelectWrapper>
+        </CreateBody>
+        {imageError && <ErrorLabel>Image is too tall!</ErrorLabel>}
+        <ImageWrapper>
+          <ImagePreview
+            source={{uri: image}}
+            style={{
+              resizeMode: 'cover',
+              width: Dimensions.get('window').width * 0.9,
+              height: imageHeight ? imageHeight : '100%',
             }}
           />
-        </SelectWrapper>
-        <FormButtonComponent
-          name="Share Post"
-          onPress={handleSubmit(handleSubmitPost)}
-          fontSize="28px"
-        />
-      </CreateBody>
-    </CreatePostWrapper>
+        </ImageWrapper>
+        <ButtonWrapper>
+          <FormButtonComponent
+            name="Share Post"
+            onPress={handleSubmit(handleSubmitPost)}
+            fontSize="28px"
+          />
+        </ButtonWrapper>
+      </CreatePostWrapper>
+    </AppScrollView>
   );
 };
 
